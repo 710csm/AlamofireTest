7a618f9adc0d0225b171d17b2f7cb343a8adc989
Choi SeungMyeong <710csm@naver.com>
2020-11-23 21:33:00 +0900

    finish

--- AlamofireTest/Network.swift
+++ AlamofireTest/Network.swift
@@ -9,5 +9,34 @@ import Foundation
 import Alamofire
 
 class Network {
-    let api
+    let apiKey = "90e6ba4e-5d7a-491b-8e2a-795013811580"
+    static let shared = Network()
+    
+    class var baseURL: String {
+        return "https://api.thecatapi.com"
+    }
+    
+    static var sessionManager: Session = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = [ : ]
+        configuration.httpAdditionalHeaders?["Accept"] = "application/json"
+        return Alamofire.Session(configuration: configuration)
+    }()
+    
+    func getRandomCatPicture(parameters: Parameters, completion: @escaping(_ result : Data) -> (Void)){
+        let url = "/v1/images/search"
+        let parameters : Parameters = parameters
+        Network.sessionManager.request(Network.baseURL + url, method: .get, parameters: parameters).responseJSON { (response) in
+            switch response.result {
+            case .success(_):
+                completion(response.data!)
+                break
+            case .failure(let error):
+                print(error)
+                print(response.data!)
+                break
+            }
+            
+        }
+    }
 }
