398f9b0bfa8a6d9b994f082af5a58b260d387038
Choi SeungMyeong <710csm@naver.com>
2020-11-23 16:16:02 +0900

    Initial Commit

---
+++ AlamofireTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireTest
+//
+//  Created by 최승명 on 2020/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
